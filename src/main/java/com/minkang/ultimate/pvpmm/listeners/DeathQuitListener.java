91e847798ed030ba861cfcbf277a5b3bf6ddd125
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-21 06:27:10 +0900

    Add files via upload

---
+++ src/main/java/com/minkang/ultimate/pvpmm/listeners/DeathQuitListener.java
@@ -0,0 +1,12 @@
+package com.minkang.ultimate.pvpmm.listeners;
+import com.minkang.ultimate.pvpmm.manager.MatchManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+public class DeathQuitListener implements Listener {
+    private final MatchManager matchManager;
+    public DeathQuitListener(MatchManager matchManager){ this.matchManager = matchManager; }
+    @EventHandler public void onDeath(PlayerDeathEvent e){ matchManager.playerDiedOrQuit(e.getEntity().getUniqueId()); }
+    @EventHandler public void onQuit(PlayerQuitEvent e){ matchManager.playerDiedOrQuit(e.getPlayer().getUniqueId()); }
+}
